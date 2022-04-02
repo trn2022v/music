3e46bd3398993b72e8d9c42fe9ca67151bdbf894
trn2022v <valentintuyrin@gmail.com>
2022-04-02 13:50:10 +0300

    323232

--- app/src/main/java/com/example/myapplication/di/modules.kt
+++ app/src/main/java/com/example/myapplication/di/modules.kt
@@ -1,19 +1,19 @@
-package com.example.myapplication.di
-
-import com.example.myapplication.data.network.service.auth.NetworkAuthService
-import com.example.myapplication.data.network.service.auth.NetworkAuthServiceImpl
-import com.example.myapplication.data.storage.LocalStorageModel
-import com.example.myapplication.data.storage.MusicStorage
-import com.example.myapplication.data.storage.UserStorage
-import com.example.myapplication.data.storage.preferances.AppPreferences
-import com.example.myapplication.data.storage.preferances.AppPreferencesImpl
-import org.koin.dsl.module
-import org.koin.dsl.single
-
-val appModules = module {
-
-    single<NetworkAuthService> { NetworkAuthServiceImpl() }
-    single<UserStorage> { LocalStorageModel() }
-    single<AppPreferences> { AppPreferencesImpl() }
-
-}
+//package com.example.myapplication.di
+//
+//import com.example.myapplication.data.network.service.auth.NetworkAuthService
+//import com.example.myapplication.data.network.service.auth.NetworkAuthServiceImpl
+//import com.example.myapplication.data.storage.LocalStorageModel
+//import com.example.myapplication.data.storage.MusicStorage
+//import com.example.myapplication.data.storage.UserStorage
+//import com.example.myapplication.data.storage.preferances.AppPreferences
+//import com.example.myapplication.data.storage.preferances.AppPreferencesImpl
+//import org.koin.dsl.module
+//import org.koin.dsl.single
+//
+//val appModules = module {
+//
+//    single<NetworkAuthService> { NetworkAuthServiceImpl() }
+//    single<UserStorage> { LocalStorageModel() }
+//    single<AppPreferences> { AppPreferencesImpl() }
+//
+//}
